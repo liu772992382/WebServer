65b17ecaf0b6822fd309d021ac50cbb61cd4d2ae
Gru <gru@GrudeMacBook-Pro.local>
2017-03-08 18:06:53 +0800

    add auth plugin

--- api.py
+++ api.py
@@ -4,13 +4,25 @@
 import os
 import hashlib
 import json
-import requests
-from flask import Flask, request, render_template, redirect,make_response, flash, session , g ,url_for, jsonify
+from flask import Flask, request, render_template, redirect,make_response, abort, \
+                session , g ,url_for, jsonify, make_response
 from datetime import datetime
 from model import *
 import time
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 def hashimage(upfile):
     fname = upfile.filename
@@ -23,13 +35,13 @@ def hashimage(upfile):
 
 #登陆验证及获取时间函数
 #-------------------------------------------------------------------------------
-def login_required(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if 'username' not in session:
-            return redirect('/shanyi/login')
-        return func(*args, **kwargs)
-    return wrapper
+# def login_required(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         if 'username' not in session:
+#             return redirect('/shanyi/login')
+#         return func(*args, **kwargs)
+#     return wrapper
 
 def get_time():
 	return time.strftime("%Y-%m-%d %X", time.localtime())
@@ -41,3 +53,39 @@ def get_activity(aid):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+
+#test
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=HOST_PORT)
